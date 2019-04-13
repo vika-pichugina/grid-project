eafd342def8eecd1aec082234f8ca6d29a5829e6
FomenkoAndrey <fomenkoandrey1978@gmail.com>
2019-04-13 13:28:48 +0300

    удалиля некоторые файлы.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,1 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
-});
+$(document).ready(function () {});
